2571adc10787f4b7c8d4e6325412e2ed64e91360
Daniel Cespedes <danielthames360@gmail.com>
2023-02-27 15:45:15 -0400

    Base project with all the dependencies

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+
+export const App = () => {
+  return (
+    <>
+      <h1>App</h1>
+      <Button variant="contained" color="success">
+        Hello world
+      </Button>
+    </>
+  );
+};
